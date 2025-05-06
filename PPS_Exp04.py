48afa63c99b4f13c60b4eef7ab73138390d4ca71
shendesharvasti <shravasti.shende.batch2024@sitnagpur.siu.edu.in>
2025-05-06 18:37:37 +0530

    Create PPS_Exp04.py

---
+++ PPS_Exp04.py
@@ -0,0 +1,8 @@
+# Write a Python program to print the area of a triangle
+
+base = float(input("Base: "))
+height = float(input("Height: "))
+
+area = 0.5 * base * height
+
+print(f"Area: {area:.2f}")
