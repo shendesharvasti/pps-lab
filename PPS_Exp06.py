5541d7876dd4ed3fca6e8036ea761d25b118718f
shendesharvasti <shravasti.shende.batch2024@sitnagpur.siu.edu.in>
2025-05-06 18:39:08 +0530

    Create PPS_Exp06.py

---
+++ PPS_Exp06.py
@@ -0,0 +1,9 @@
+# Write a Python program to find the largest of three numbers.
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+largest = max(num1,num2,num3)
+
+print("Largest number:",largest)
